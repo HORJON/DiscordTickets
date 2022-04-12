a11ebe890d169b8e2aff77c93cefd894b8a268b9
Mira <67387468+HORJON@users.noreply.github.com>
2022-04-12 20:59:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     dsc: {
-        token: ''
+        token: 'OTYyNjIzMjk1NDc4NDM5OTM3.YlKOzg.GEwUMVSESW24FhYgFJ0cx7Feh88'
     },
 
     app: {
-        prefix: '!'
+        prefix: 't!'
     }
 };
